bc859be55058268a95eb133fd1798a6bdd911b70
Thanh Tran <vanthanh2305@gmail.com>
2016-05-26 23:37:00 +0700

    init blog

---
+++ blog/app/app/app.component.js
@@ -0,0 +1,14 @@
+(function(app) {
+  app.NgxBlogComponent =
+    ng.core.Component({
+      selector: 'ngx-blog',
+      templateUrl: 'blog/app/app/app.html',
+      directives: [
+      ],
+      providers: [
+      ]
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
