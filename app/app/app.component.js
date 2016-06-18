eba9345cdf56d7d985603d5f01b2e81717373899
Thanh Tran <vanthanh2305@gmail.com>
2016-06-18 21:50:18 +0700

    responsive

--- app/app/app.component.js
+++ app/app/app.component.js
@@ -1,4 +1,4 @@
-(function(app) {
+(function (app) {
   app.AppComponent =
     ng.core.Component({
       selector: 'ngx-app',
@@ -7,13 +7,40 @@
         app.ContentComponent,
         app.HeaderComponent,
         app.SidebarComponent,
-        ng.router_deprecated.ROUTER_DIRECTIVES 
+        ng.router_deprecated.ROUTER_DIRECTIVES
       ],
       providers: [
         ng.router_deprecated.ROUTER_PROVIDERS,
       ]
     })
     .Class({
-      constructor: function() {}
+      constructor: function () {
+
+      },
+      ngAfterViewInit: function () {
+        $('#toggle-side-bar').click(function () {
+          $('#ngx-sidebar-left').toggleClass('active');
+          $('ngx-backdrop').toggleClass('active');
+        });
+
+        $('body').click(function () {
+          var ngxSidebarLeft = $(event.target).parents('#ngx-sidebar-left');
+          if ($(event.target).attr('id') != 'toggle-side-bar' && ngxSidebarLeft.attr('id') != 'ngx-sidebar-left' && $('#ngx-sidebar-left').hasClass('active')) {
+            $('#ngx-sidebar-left').removeClass('active');
+            $('ngx-backdrop').removeClass('active');
+          }
+        });
+
+        $(window).resize(function () {
+          setPaddingContent();
+        });
+
+        function setPaddingContent() {
+          var navbarHeight = $('.navbar').height();
+          $('.ngx-main-content').css('padding-top', (navbarHeight+30) + 'px');
+        }
+
+        setPaddingContent();
+      }
     });
 })(window.app || (window.app = {}));
