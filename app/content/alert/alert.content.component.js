8ceb480cff2c97e196255eab798a88ce7633bb58
nuong.nguyen <nuonght3599@gmail.com>
2016-06-29 23:11:00 +0700

    add child route for content

--- app/content/alert/alert.content.component.js
+++ app/content/alert/alert.content.component.js
@@ -1,7 +1,7 @@
 (function(app) {
-  app.ButtonContentComponent =
+  app.AlertContentComponent =
     ng.core.Component({
-      templateUrl: 'app/content/button/button.content.html',
+      templateUrl: 'app/content/alert/alert.content.html',
       directives: [
             ng.router_deprecated.RouterOutlet,
             ng.router_deprecated.RouterLink
@@ -13,11 +13,11 @@
     .Class({
             constructor: [ng.router_deprecated.Router, function (router) {
                 this.router = router;
-                console.log("Button Content");
+                console.log("Alert Content");
             }]
         });
     ng.router_deprecated.RouteConfig([
-        { path: '/', name: 'ExampleContent', component: app.ButtonExampleComponent, useAsDefault: true },
-        { path: '/api', name: 'ApiContent', component: app.ButtonApiComponent }
-    ])(app.ButtonContentComponent)
+        { path: '/', name: 'ExampleContent', component: app.AlertExampleComponent, useAsDefault: true },
+        { path: '/api', name: 'ApiContent', component: app.AlertApiComponent }
+    ])(app.AlertContentComponent)
 })(window.app || (window.app = {}));
